964fc2630a757585d6c923cc57f78096357f4272
Wintermute <Wintermute@Straylight.ca>
2014-09-08 14:42:16 -0700

    Added a contact page Added a full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "HoRoR" }
+
   describe "Home page" do
     it "should have the content 'home page'" do
         visit '/static_pages/home'
@@ -10,7 +13,12 @@ describe "StaticPages" do
 
   it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("HoRoR | Home")
+      expect(page).to have_title("#{base_title}")
+  end
+
+  it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
   end
 
   describe "Help page" do
@@ -22,7 +30,7 @@ describe "StaticPages" do
 
   it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("HoRoR | Help")
+      expect(page).to have_title("#{base_title} | Help")
   end
 
   describe "About page" do
@@ -34,7 +42,19 @@ describe "StaticPages" do
 
   it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("HoRoR | About")
+      expect(page).to have_title("#{base_title} | About")
+  end
+
+  describe "Contact page" do
+      it "should have the content 'Contact'" do
+          visit '/static_pages/contact'
+          expect(page).to have_content('Contact')
+      end
+  end
+
+  it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
   end
 
 end
